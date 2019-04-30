bf782c483fe0a0f90a94e2a7bd531e1e62042816
Robert Gant <demontegant@gmail.com>
2019-04-29 22:14:00 -0500

    worked on finishing the responsiveness almost finished

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 function openSlideMenu(){
-  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('side-menu').style.width = '14%';
 }
 
 function closeSlideMenu(){
